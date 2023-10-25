ecf71ee41193c75db300a23bf930918e947872b0
dani <llollillu@hotmail.com>
2023-10-25 08:50:57 -0500

    he actualizado el fronted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 
     <div id="app">
     <header>
-        <img src="logo-farmacia.png" class="header1 img1">
+        <img src="{{asset ('logo-farmacia.png')}}" class="header1 img1">
            <div class="titulo"> <h1 class="header1" > FARMACIA ZEUS </h1> </div>
            <div class = "wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path></svg></div>
                   
@@ -63,7 +63,7 @@
 
                      <a class="nav-link " href="{{ route('inventario.index') }}">{{ __('inventario') }}</a>
 
-                     <a class="nav-link " href="{{ route('inventario.index') }}">{{ __('tienda') }}</a>
+                    
                      </li>
 
 
